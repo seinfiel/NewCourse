687b66e6641c4fb7b0d33847a6cba2084801ac0e
vinodgit <hello@vinodcode.com>
2023-07-08 18:11:12 +0530

    fixed bug-121

--- src/main/java/in/AshokProject/App.java
+++ src/main/java/in/AshokProject/App.java
@@ -6,6 +6,11 @@ package in.AshokProject;
  */
 public class App 
 {
+	public void process() {
+		//big fix 201
+		String name = "raju";
+		System.out.println("hiiiii");
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
